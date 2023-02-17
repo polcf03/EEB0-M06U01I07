f22f0363987865b3d5c2050fe0a7ad1452916c32
polcf03 <polcf07@gmail.com>
2023-02-17 19:56:52 +0100

    EEB0-M06U01I07_v0.1

--- EEB0-M06U01I07-Con. AC3/Com. AC3/Form1.Designer.cs
+++ EEB0-M06U01I07-Con. AC3/Com. AC3/Form1.Designer.cs
@@ -28,70 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btOff = new System.Windows.Forms.Button();
-            this.btOn = new System.Windows.Forms.Button();
-            this.btDisonect = new System.Windows.Forms.Button();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.btConect = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.btRight = new System.Windows.Forms.Button();
+            this.btOLeft = new System.Windows.Forms.Button();
+            this.btStrStp = new System.Windows.Forms.Button();
+            this.trbVelocity = new System.Windows.Forms.TrackBar();
+            this.btConDis = new System.Windows.Forms.Button();
+            this.txtVelocity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trbVelocity)).BeginInit();
             this.SuspendLayout();
             // 
-            // btOff
+            // btRight
             // 
-            this.btOff.Location = new System.Drawing.Point(510, 125);
-            this.btOff.Name = "btOff";
-            this.btOff.Size = new System.Drawing.Size(75, 75);
-            this.btOff.TabIndex = 0;
-            this.btOff.Text = "button1";
-            this.btOff.UseVisualStyleBackColor = true;
+            this.btRight.Location = new System.Drawing.Point(107, 93);
+            this.btRight.Name = "btRight";
+            this.btRight.Size = new System.Drawing.Size(69, 75);
+            this.btRight.TabIndex = 0;
+            this.btRight.UseVisualStyleBackColor = true;
+            this.btRight.Click += new System.EventHandler(this.btRight_Click);
             // 
-            // btOn
+            // btOLeft
             // 
-            this.btOn.Location = new System.Drawing.Point(429, 125);
-            this.btOn.Name = "btOn";
-            this.btOn.Size = new System.Drawing.Size(75, 75);
-            this.btOn.TabIndex = 1;
-            this.btOn.Text = "button2";
-            this.btOn.UseVisualStyleBackColor = true;
+            this.btOLeft.Location = new System.Drawing.Point(12, 93);
+            this.btOLeft.Name = "btOLeft";
+            this.btOLeft.Size = new System.Drawing.Size(75, 75);
+            this.btOLeft.TabIndex = 1;
+            this.btOLeft.UseVisualStyleBackColor = true;
+            this.btOLeft.Click += new System.EventHandler(this.btOLeft_Click);
             // 
-            // btDisonect
+            // btStrStp
             // 
-            this.btDisonect.Location = new System.Drawing.Point(429, 44);
-            this.btDisonect.Name = "btDisonect";
-            this.btDisonect.Size = new System.Drawing.Size(156, 75);
-            this.btDisonect.TabIndex = 2;
-            this.btDisonect.Text = "button3";
-            this.btDisonect.UseVisualStyleBackColor = true;
+            this.btStrStp.Location = new System.Drawing.Point(12, 12);
+            this.btStrStp.Name = "btStrStp";
+            this.btStrStp.Size = new System.Drawing.Size(164, 75);
+            this.btStrStp.TabIndex = 2;
+            this.btStrStp.UseVisualStyleBackColor = true;
+            this.btStrStp.Click += new System.EventHandler(this.btStrStp_Click);
             // 
-            // trackBar1
+            // trbVelocity
             // 
-            this.trackBar1.Location = new System.Drawing.Point(429, 206);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(156, 56);
-            this.trackBar1.TabIndex = 3;
+            this.trbVelocity.Location = new System.Drawing.Point(12, 219);
+            this.trbVelocity.Maximum = 100;
+            this.trbVelocity.Name = "trbVelocity";
+            this.trbVelocity.Size = new System.Drawing.Size(164, 56);
+            this.trbVelocity.TabIndex = 3;
+            this.trbVelocity.TickFrequency = 10;
+            this.trbVelocity.ValueChanged += new System.EventHandler(this.trbVelocity_ValueChanged);
             // 
-            // btConect
+            // btConDis
             // 
-            this.btConect.Location = new System.Drawing.Point(429, 44);
-            this.btConect.Name = "btConect";
-            this.btConect.Size = new System.Drawing.Size(156, 75);
-            this.btConect.TabIndex = 4;
-            this.btConect.Text = "button4";
-            this.btConect.UseVisualStyleBackColor = true;
+            this.btConDis.Location = new System.Drawing.Point(12, 411);
+            this.btConDis.Name = "btConDis";
+            this.btConDis.Size = new System.Drawing.Size(156, 75);
+            this.btConDis.TabIndex = 5;
+            this.btConDis.UseVisualStyleBackColor = true;
+            this.btConDis.Click += new System.EventHandler(this.btConDis_Click);
+            // 
+            // txtVelocity
+            // 
+            this.txtVelocity.Location = new System.Drawing.Point(12, 191);
+            this.txtVelocity.Name = "txtVelocity";
+            this.txtVelocity.Size = new System.Drawing.Size(164, 22);
+            this.txtVelocity.TabIndex = 6;
+            this.txtVelocity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtVelocity.TextChanged += new System.EventHandler(this.txtVelocity_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Velocity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(104, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "%";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(628, 498);
-            this.Controls.Add(this.btConect);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.btDisonect);
-            this.Controls.Add(this.btOn);
-            this.Controls.Add(this.btOff);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVelocity);
+            this.Controls.Add(this.btConDis);
+            this.Controls.Add(this.trbVelocity);
+            this.Controls.Add(this.btStrStp);
+            this.Controls.Add(this.btOLeft);
+            this.Controls.Add(this.btRight);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbVelocity)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -99,11 +136,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btOff;
-        private System.Windows.Forms.Button btOn;
-        private System.Windows.Forms.Button btDisonect;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Button btConect;
+        private System.Windows.Forms.Button btRight;
+        private System.Windows.Forms.Button btOLeft;
+        private System.Windows.Forms.Button btStrStp;
+        private System.Windows.Forms.TrackBar trbVelocity;
+        private System.Windows.Forms.Button btConDis;
+        private System.Windows.Forms.TextBox txtVelocity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- EEB0-M06U01I07-Con. AC3/Com. AC3/Form1.cs
+++ EEB0-M06U01I07-Con. AC3/Com. AC3/Form1.cs
@@ -6,14 +6,54 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Com.AC3
 {
     public partial class Form1 : Form
     {
+        static SerialPort sPort;
+        string[] Ports;
         public Form1()
         {
             InitializeComponent();
+            sPort = new SerialPort();
+        }
+
+
+
+        private void btConDis_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btOLeft_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btRight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btStrStp_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void txtVelocity_TextChanged(object sender, EventArgs e)
+        {
+            trbVelocity.Value = Int32.Parse(txtVelocity.Text);
+        }
+        private void trbVelocity_ValueChanged(object sender, EventArgs e)
+        {
+           txtVelocity.Text= trbVelocity.Value.ToString();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
         }
     }
 }
